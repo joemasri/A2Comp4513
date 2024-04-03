0aa05096744a7ea99f1dd529a669a989c04270e6
Joe Masri <zelma760@mtroyal.ca>
2024-04-03 06:57:21 -0600

    Add results and standings button, not yet functional

--- assignment2-react-app/src/components/views/HomeView1/HomeView1.jsx
+++ assignment2-react-app/src/components/views/HomeView1/HomeView1.jsx
@@ -4,6 +4,7 @@ import { fetchSeasons, fetchRaces } from '../../../Api';
 
 const HomeView1 = () => {
 
+    // Store selected variables in state
     const [selectedSeason, setSelectedSeason] = useState(''); 
     const [seasons, setSeasons] = useState([]);
     const [races, setRaces] = useState([]);
@@ -29,43 +30,43 @@ const HomeView1 = () => {
     }, [selectedSeason]);
 
     return (
-        <div>
-            <Header />
-            <div className="container">
-                <div>
-                        {/* Select season dropdown */}
-                        <div>
-                            <select className="border border-gray-300 rounded px-3 py-1 mt-4"
-                                value={selectedSeason}
-                                onChange={(e) => setSelectedSeason(e.target.value)}
-                            >
-                                <option value="">Select a season</option>
-                                {seasons.map((season) => (
-                                    <option key={season.year} value={season.year}>
-                                        {season.year}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
+    <div>
+        <Header />
+        <div className="container">
+            <div>
+                    {/* Select season dropdown */}
+                    <div>
+                        <select className="border border-gray-300 rounded px-3 py-1 mt-4"
+                            value={selectedSeason}
+                            onChange={(e) => setSelectedSeason(e.target.value)}
+                        >
+                            <option value="">Select a season</option>
+                            {seasons.map((season) => (
+                                <option key={season.year} value={season.year}>
+                                    {season.year}
+                                </option>
+                            ))}
+                        </select>
                     </div>
-                        {/* Races based on selected season */}
-                        <div className="mt-4 p-3" style={{ width: '33%', border: '1px solid black', textAlign: 'left'}}>
-                            <h2 className="text-lg font-semibold">Races for Season {selectedSeason}</h2>
-                            <ul>
-                                {races.map((race, indx) => (
-                                    <li key={indx}>
-                                        {race.round} - {race.name}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                {/* Display races, results, and standings for the selected season */}
-                {/* When results button clicked, display results section with more information about race (eg. race name, round# ,etc) */}
-
-
+                </div>
+                    {/* Races based on selected season */}
+                    <div className="mt-4 p-3 w-1/3 border border-black text-left">
+                        <h2 className="text-lg font-semibold">Races for Season {selectedSeason}</h2>
+                        <ul>
+                            {races.map((race, indx) => (
+                               <li key={indx} className="flex items-center justify-between">
+                                    {race.round} - {race.name}
+                                <div className="flex">
+                                    <button className="p-2 m-1 text-white bg-gray-400 rounded">Results</button>
+                                    <button className="p-2 m-1 text-white bg-gray-400 rounded">Standings</button>
+                                </div>
+                               </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
 export default HomeView1;
